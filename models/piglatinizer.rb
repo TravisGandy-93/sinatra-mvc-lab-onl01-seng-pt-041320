7829f6d9da641aae21d84d233a5a3dd4418feb83
Travis Gandy <travis_gandy@yahoo.com>
2020-08-02 23:35:42 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,11 @@ class PigLatinizer
     b.join(" ")
   end
   
-  
+  def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if first_letter == "a" || first_letter == "e" || first_letter == "i" || first_letter == "o" || first_letter == "u"
+        # piglatinize word that starts with a vowel
+    else
   
   
 end 
