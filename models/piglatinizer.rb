827244e77d1843821e365c0a8a65b9f7e5e2252f
Travis Gandy <travis_gandy@yahoo.com>
2020-08-02 23:28:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,10 @@
 class PigLatinizer
   attr_reader :text 
   
-  
+  def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+end
   
 end 
