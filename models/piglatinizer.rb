8e5a17cb39b0a44aea3f94669d82e84c4b03cde9
Travis Gandy <travis_gandy@yahoo.com>
2020-08-02 23:31:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,12 +2,16 @@ class PigLatinizer
   attr_reader :text 
   
   def initialize(text)
-    @text
+    @text = text.downcase
+  end 
   
-  def piglatinize(text)
+  def piglatinize_string
     a = text.split(" ")
     b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
-end
+  end
+  
+  
+  
   
 end 
