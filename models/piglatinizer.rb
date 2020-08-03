2fd932c06873cc33da575c424ec23a08952d2c3f
Travis Gandy <travis_gandy@yahoo.com>
2020-08-03 00:38:57 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,13 +5,13 @@ class PigLatinizer
     @text = text 
   end 
   
-  def piglatinize_string(text)
+  def piglatinize(text)
     a = text.split(" ")
-    b = a.map {|word| piglatinize(word)}
+    b = a.map {|word| piglatinize_word(word)}
     b.join(" ")
   end
   
-  def piglatinize(word)
+  def piglatinize_word(word)
     first_letter = word[0].downcase
     if ["a", "e", "i", "o", "u"].include?(first_letter)
         "#{word}way"
